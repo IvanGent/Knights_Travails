5c4d3e767a4275174c23d449b15ef6474aadda10
James Lee <jamestlee513@gmail.com>
2020-12-03 15:01:52 -0800

    Fix bug

--- tree.py
+++ tree.py
@@ -12,21 +12,50 @@ class Node:
     def children (self):
         return self._children
 
-    def add_child(self, node):
-        if node not in self._children:
-            self._children.append(node)
-            node.parent = self
-
     @property
     def parent(self):
         return self._parent
 
     @parent.setter
-    def parent(self, value):
-        self._parent = value
-        if value is not None:
-            self.parent.add_child(self)
+    def parent(self, node): 
+        if self._parent is node:
+            return
+        if self._parent is not None:
+            self._parent.remove_child(self)
+        self._parent = node
+        if node is not None:
+            node.add_child(self)
+
+    def add_child(self, node):
+        if node not in self.children:
+            self._children.append(node)
+            if node.parent != self:
+                node.parent = self
 
     def remove_child(self, node):
-        self._children.remove(node)
-        node.parent = None
+        if node in self._children:
+            self._children.remove(node)
+            node.parent = None
+
+node1 = Node("root1")
+print(node1.parent)
+print(node1.child)
+print(node1.value)
+# node2 = Node("root2")
+# node3 = Node("root3")
+# '''
+# node1, [value="root1", parent=None, children=[]]
+# node2, [value="root2", parent=None, children=[]]
+# node3, [value="root3", parent=None, children=[]]
+
+# node1, [value="root1", parent=None, children=[node3]]
+# node3, [value="root3", parent=node1, children=[]]
+# parentSetter (self=node3, value=node1)
+# '''
+ 
+
+# node3.parent = node1
+# node3.parent = node2
+
+# print(node1.children)
+# print(node2.children)
