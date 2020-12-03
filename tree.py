423ee0ea406ffb3801d56dd9685d685f84115114
IvanGent <gentilleivan@gmail.com>
2020-12-03 16:40:21 -0500

    reassign parents

--- tree.py
+++ tree.py
@@ -3,7 +3,7 @@ class Node:
         self._value = value
         self._parent = None
         self._children = []
-    
+
     @property
     def value (self):
         return self._value
@@ -14,9 +14,8 @@ class Node:
 
     def add_child(self, node):
         if node not in self._children:
-            self._childen.append(node)
+            self._children.append(node)
             node.parent = self
-        
 
     @property
     def parent(self):
@@ -24,10 +23,10 @@ class Node:
 
     @parent.setter
     def parent(self, value):
+        self._parent = value
         if value is not None:
-            self._parent = value
-    
+            self.parent.add_child(self)
+
     def remove_child(self, node):
         self._children.remove(node)
         node.parent = None
-    
